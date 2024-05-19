e5530873f3cfd6d5dd425d763672f0664bdc7581
Nguyễn Đức Thắng <120944583+Mrk4tsu@users.noreply.github.com>
2024-05-19 17:05:50 +0700

    Thêm xử lý ngoại lệ, Response Code 4xx

---
+++ CodeRumWebBlog/Controllers/ErrorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeRumWebBlog.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            return View("BadRequest");
+        }
+        public ActionResult Unauthorized()
+        {
+            Response.StatusCode = 401;
+            return View("Unauthorized");
+        }
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            return View("Forbidden");
+        }
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+        public ActionResult MethodNotAllowed()
+        {
+            Response.StatusCode = 405;
+            return View("MethodNotAllowed");
+        }
+    }
+}
